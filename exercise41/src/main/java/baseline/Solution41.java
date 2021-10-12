c6f7ecb3822bd84865a97e34cc88e5bea4337b88
Alexander <azoomer2@gmail.com>
2021-10-12 11:33:16 -0400

    E41 Skeleton Push

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -1,4 +1,7 @@
 package baseline;
+
+import java.util.ArrayList;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 4 Solutions
  *  Copyright 2021 Alexander Zommer
@@ -6,5 +9,32 @@ package baseline;
 public class Solution41 {
 
 
+    public ArrayList nameSorter(ArrayList names)
+    {
+        //Take input arraylist
+        //Sort using collections.sort
+        //return sorted arraylist
+
+    }
+
+    private void printNames(ArrayList names)
+    {
+        //print out the supplied array list
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        readInput in = new readInput();
+
+        //take input from readInput class
+        //nameSorter(array)
+        //printNames(array)
+
+    }
 
 }
