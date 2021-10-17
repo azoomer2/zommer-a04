876cb528502dccffb9ec2276cdb605eeabc632c7
Alexander <azoomer2@gmail.com>
2021-10-17 18:35:04 -0400

    E46 Skeleton Commit

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,14 @@
+package baseline;
+
+public class Solution46 {
+
+    public static void main(String[] args) {
+        //read first word from file
+        //search file for occurrences of that word
+            //by calling countEntries
+        //save search word and count to list
+        //move to second word
+        //if word is in list, ignore and move on
+        //if not, loop back to start
+    }
+}
---
+++ exercise46/src/main/java/baseline/fileReader.java
@@ -0,0 +1,15 @@
+package baseline;
+
+import java.util.List;
+
+public class fileReader {
+    public int countEntries(String searchIndex, List list)
+    {
+        //Go through words one by one
+        //if temp = searchIndex,
+            //counter++
+
+        //return counter;
+
+    }
+}
