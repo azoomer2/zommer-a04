4534dacdb5a448bc831ccd3b075c844df5164c38
Alexander <azoomer2@gmail.com>
2021-10-17 11:24:53 -0400

    E42 Skeleton Commit

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,30 @@
+package baseline;
+
+public class Solution42 {
+
+    private void csvScanner()
+    {
+        //Create new Scanner instance
+        //set delimiter to ,
+        //while loop for scanner having values
+            //save next 3 values to fName, lName, salary
+
+
+    }
+
+    private void tablePrinter()
+    {
+        //format and print output of csvScanner
+
+    }
+
+
+    public static void main(String[] args) {
+
+        //call csvScanner()
+
+        //call tablePrinter()
+
+    }
+
+}
