37c7315b4cb113348c3200579557798be0cbca1c
Alexander <azoomer2@gmail.com>
2021-10-17 14:37:28 -0400

    E43 Skeleton Commit

---
+++ exercise43/src/main/java/baseline/IO.java
@@ -0,0 +1,23 @@
+package baseline;
+
+public class IO {
+
+    private void takeInput()
+    {
+        //Take input for name of site
+        //Take input for author
+        //y or n for javascript files
+        //y or n for css files
+
+
+    }
+
+    private void printOutput()
+    {
+        //print if folder is created
+        //print if html is created
+        //print if js
+        //print if css
+    }
+
+}
--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -2,5 +2,19 @@ package baseline;
 
 public class Solution43 {
 
+    public void createWebsite()
+    {
+        //create website folder
+        //create .html file
+        //if javascript
+            //create js folder
+        //if css
+            //create css folder
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 
 }
